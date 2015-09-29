2490a6eb254d9f9bf5e4b39dbf6f8f9540ab2af4
jsking <jsking@ucdavis.edu>
2015-09-29 01:49:25 -0700

    Finish barebones static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home - Hearth Helper"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact - Hearth Helper"
+  end
+
+end
