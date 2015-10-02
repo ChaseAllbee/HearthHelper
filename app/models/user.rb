b996a309b5a019550fc88e20991a26331289d6c4
jsking <jsking@ucdavis.edu>
2015-10-01 23:15:46 -0700

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :username, presence:true, length: { maximum: 30 },
                        uniqueness: { case_sensitive: false }
@@ -8,4 +9,33 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # Returns hash digest of string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers user in database for persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Removes user remember digest
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root             'static_pages#home'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  root                'static_pages#home'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
