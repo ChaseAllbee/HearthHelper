e6506b7a51a2c45892c2db35ecf553bcae0f600a
jonathansking <jsking@ucdavis.edu>
2015-11-13 13:24:25 -0800

    Create basic logged in home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @user = User.find(current_user)
-      render 'users/show'
-    end
+      render 'logged_in_home'
+		else
+			render 'logged_out_home'
+		end
   end
 
   def contact
