3371dbcf7abb3f5613e7a93193fb6406ac1453aa
jonathansking <jsking@ucdavis.edu>
2015-11-13 16:11:05 -0800

    Add associations between user and external decks

--- app/models/external_deck.rb
+++ app/models/external_deck.rb
@@ -1,5 +1,7 @@
 class ExternalDeck < ActiveRecord::Base
   has_many :external_deck_instances
   has_many :cards, through: :external_deck_instances
+  has_many :saved_external_decks
+  has_many :users, through: :saved_external_decks
   validates :name, presence: true, uniqueness: true
 end
