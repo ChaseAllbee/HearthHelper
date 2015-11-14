c52a27e585bb9f905e3ea0a1218eda0c0c17b221
Chase <chaseallbee@gmail.com>
2015-11-13 17:30:35 -0800

    Update home page and static pages controller
    Added ruby loop in home to populate tracked decks. Tweaker mobile layout and added a line to get data from controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @user = User.find(current_user)
+      @tracked_decks = @user.external_decks
       render 'logged_in_home'
 		else
 			render 'logged_out_home'
