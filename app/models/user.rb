d7b7cd191bd5e93272a3bd9ddd94d7f1569c15ff
jonathansking <jsking@ucdavis.edu>
2015-11-18 15:21:11 -0800

    Remove account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   has_many :saved_external_decks
   has_many :external_decks, through: :saved_external_decks
   before_save   :downcase_email
-  before_create :create_activation_digest
   VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/
   validates :username, presence: true, length: { maximum: 20 },
                        format: { with: VALID_USERNAME_REGEX },
@@ -46,17 +45,6 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  # Activates an account
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
-  # Sends activation email
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
   # Sets the password reset attributes
   def create_reset_digest
     self.reset_token = User.new_token
@@ -84,11 +72,5 @@ class User < ActiveRecord::Base
     # Converts email to all lower-case
     def downcase_email
       self.email = email.downcase
-    end
-
-    # Creates and assigns the activation token and digest
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
     end
 end
