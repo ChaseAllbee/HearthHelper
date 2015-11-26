f4a55d5f2d08811fd373a3521f49a204beaf8a14
jonathansking <jsking@ucdavis.edu>
2015-11-26 02:04:36 -0800

    Finished home page data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,16 @@
 class StaticPagesController < ApplicationController
+	include StaticPagesHelper
+
   def home
     if logged_in?
-      @user = User.find(current_user)
+    	@num_owned = []
+    	@dust_to_craft = []
+      @user = User.find(current_user.id)
       @tracked_decks = @user.external_decks
+      @tracked_decks.each do |tracked_deck|
+      	@num_owned << num_cards_owned(tracked_deck)
+      	@dust_to_craft << dust_needed(tracked_deck)
+      end
       render 'logged_in_home'
 		else
 			render 'logged_out_home'
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,78 @@
 module StaticPagesHelper
+
+	# Retrieves cards that are in both deck and collection where
+	# deck qty <= collection qty (since collection can have more than 2 instances)
+	def quantity_same_query(coll_id, tracked_deck_id)
+		Card.joins(:external_deck_instances).joins(:collection_card_instances)
+				.where("collection_id = ? and external_deck_id = ? and
+								external_deck_instances.quantity <= 
+								collection_card_instances.quantity",
+								coll_id, tracked_deck_id
+							)
+	end
+
+	# Retrieves cards that are in both deck and collection where
+	# deck qty != collection qty
+	def quantity_diff_query(coll_id, tracked_deck_id)
+		Card.joins(:external_deck_instances).joins(:collection_card_instances)
+				.where("collection_id = ? and external_deck_id = ? and 
+								external_deck_instances.quantity != 
+								collection_card_instances.quantity",
+								coll_id, tracked_deck_id
+							)
+	end
+
+	# Retrieves cards that are in the deck but not collection
+	def deck_not_collection_query(coll_id, tracked_deck_id)
+		cards_in_collection = Card.joins(:collection_card_instances)
+															.where("collection_id = ?", coll_id)
+									 						.select("cards.id").to_sql
+		Card.joins(:external_deck_instances)
+				.where("external_deck_id = ? and 
+								cards.id NOT IN (#{cards_in_collection})", tracked_deck_id
+							)
+	end
+
 	# Totals number of cards missing for tracked deck
-	def num_cards_missing
-		
+	def num_cards_owned(tracked_deck)
+		@quantity_same_cards = quantity_same_query(coll_id, tracked_deck_id(tracked_deck))
+		@quantity_diff_cards = quantity_diff_query(coll_id, tracked_deck_id(tracked_deck))
+		@quantity_same_cards.count + @quantity_diff_cards.count
 	end
 
 	# Calculates dust needed to craft remainder of deck
-	def dust_needed
+	def dust_needed(tracked_deck)
+		@deck_not_collection_cards = deck_not_collection_query(coll_id, tracked_deck_id(tracked_deck))
+		dust_total(@quantity_diff_cards) + dust_total(@deck_not_collection_cards)
+	end
+
+	# Returns user's collection ID
+	def coll_id
+		current_user.collection.id
+	end
+
+	# Returns tracked deck ID
+	def tracked_deck_id(tracked_deck)
+		tracked_deck.id
+	end
+
+	def dust_total(cards)
+		cards.inject(0) do |dust_total, card|
+			dust_total + dust_value(card.rarity)
+		end
+	end
+
+	# Returns the dust value associated with card's rarity
+	def dust_value(card_rarity)
+		case card_rarity
+		when "Common"
+			return 40
+		when "Rare"
+			return 100
+		when "Epic"
+			return 400
+		when "Legendary"
+			return 1600
+		end
 	end
 end
