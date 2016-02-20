d691552968e2b272488fd567b1e9249123ff4525
jsking <jsking@ucdavis.edu>
2016-02-20 01:43:47 -0800

    Add new deck page and external decks pulled correctly

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,9 @@
 class DecksController < ApplicationController
   def index
+    month = Date.today.strftime("%B")
+    @meta_decks = ExternalDeck.where(tier: 1).where("name LIKE ?", "%#{month}%")
+  end
+
+  def new
   end
 end
