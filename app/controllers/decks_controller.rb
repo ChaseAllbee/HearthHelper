484bd53341455fff62039988ce9ab69c90528979
Chase Allbee <chaseallbee@gmail.com>
2016-02-21 20:07:06 -0800

    pull other db and modify deck page to work

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,8 +6,6 @@ class DecksController < ApplicationController
     @meta_decks = ExternalDeck.where(tier: 1).where("name LIKE ?", "%#{month}%")
     @num_owned = []
     @dust_to_craft = []
-    @user = User.find(current_user.id)
-    @meta_decks = @user.external_decks
     @meta_decks.each do |meta_deck|
     	@num_owned << num_cards_owned(meta_deck)
     	@dust_to_craft << dust_needed(meta_deck)
