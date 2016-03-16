a5a5bfdea31d4e24641844d088ef0a27dca38c6f
jsking <jsking@ucdavis.edu>
2016-03-15 23:56:10 -0700

    Change controller and helper for collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,6 @@
 class CollectionsController < ApplicationController
   def show
-    @collection ||= current_user.collection
+    @collection = current_user.collection
     @normal_cards = normal_page_cards
     @collection.current_class = @normal_cards.first.card_class
     @cards = last_page_in_class? ? last_class_page_cards : @normal_cards
