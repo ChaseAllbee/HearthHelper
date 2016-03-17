74d0f3f7891203d7131e7099232b56056764b86b
jsking <jsking@ucdavis.edu>
2016-03-17 01:40:55 -0700

    Remove unnecessary code from collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,11 +3,6 @@ class CollectionsController < ApplicationController
     @collection = current_user.collection
     @current_class = params[:current_class] || "Druid"
     gon.current_class = @current_class
-    if params[:prev_or_next_class] == "next"
-      @cards = page_cards_with_class(@current_class)
-      params.delete :prev_or_next_class
-    else
-      @cards = page_cards_with_class(@current_class)
-    end
+    @cards = page_cards_with_class(@current_class)
   end
 end
