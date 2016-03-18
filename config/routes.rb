575c38186eb8e37ebeadabc89033c51cb2edbaf1
jsking <jsking@ucdavis.edu>
2016-03-17 18:27:59 -0700

    Remove old, extra folders

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-  resources :password_resets,      only: [:new, :create, :edit, :update]
   resources :cards,                only: [:new, :create]
   resources :decks
   resources :saved_external_decks, only: [:create, :destroy]
